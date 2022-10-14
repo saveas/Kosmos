1daa3ad619e4c32f4ee56bb9e4af5fccb4ae7bf5
saveas <paravantinos@gmail.com>
2022-10-15 00:51:21 +0300

    various improvements

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -9,7 +9,9 @@ func _ready():
 
 func _physics_process(delta):
 	velocity = velocity.move_toward((get_tree().get_nodes_in_group("SpaceShip")[0].position - position).normalized(),1)
-	var _collision = move_and_collide(velocity * delta * speed)
+	var collision = move_and_collide(velocity * delta * speed)
+	if collision:
+		velocity = velocity.bounce(collision.normal)
 
 func reduce_health(amount):
 	health -= amount
--- Scripts/World.gd
+++ Scripts/World.gd
@@ -10,12 +10,13 @@ func _ready():
 	add_child(timer)
 	timer.start()
 
+
 func spawn_enemy():
 	var is_valid_spawn_pos = false
 	var enemy =  Enemy.instance()	
 	var camera_pos = get_node("Spaceship/Camera2D").get_camera_position()
 	while not is_valid_spawn_pos:
-		enemy.position=Vector2(rng.randi_range(camera_pos.x-600, camera_pos.x+600),rng.randi_range(camera_pos.y-600, camera_pos.y+600))
-		if enemy.position.distance_to(camera_pos)>400:
+		enemy.position=Vector2(rng.randi_range(camera_pos.x-300, camera_pos.x+300),rng.randi_range(camera_pos.y-600, camera_pos.y+600))
+		if enemy.position.distance_to(camera_pos)>200:
 			get_parent().add_child(enemy)
 			is_valid_spawn_pos = true
